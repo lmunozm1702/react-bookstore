4815c48836c78c9643d995d55c04f8a98a949432
Luis Muñoz <l.munoz.m@outlook.com>
2022-12-08 08:46:57 -0300

    Project setup
    - create components folder.
    - remove files don't used from CRA
    - Add navbar
    - Add Books and Categories components.

---
+++ src/components/Books.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Books = () => (
+  <div className="books">
+    <h3>
+      Books page
+    </h3>
+  </div>
+);
+export default Books;
---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Categories = () => (
+  <div className="categories">
+    <h3>
+      Under construction.
+    </h3>
+  </div>
+);
+export default Categories;
